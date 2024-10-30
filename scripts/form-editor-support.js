ac08a1319e29020cdc4a2c47097aeac8142cdce8
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-10-30 17:01:33 +0530

    removing code

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -346,15 +346,7 @@ export function attachEventListners(main) {
   document.body.addEventListener('aue:ui-edit', ueEditModeHandler);
 }
 
-function enableUEExtension() {
-  let head = document.getElementsByTagName('head')[0];
-  var meta = document.createElement('meta');
-  meta.name = "urn:adobe:aue:config:extensions";
-  meta.content = "https://283250-684greenantelope-stage.adobeio-static.net";
-  head.appendChild(meta);
-}
 
 const observer = new MutationObserver(instrumentForms);
 observer.observe(document, { childList: true, subtree: true, attributeFilter: ['form'] });
 loadCSS(`${window.hlx.codeBasePath}/scripts/form-editor-support.css`);
-enableUEExtension();
